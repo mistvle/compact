edfc7caeab0020505689f55274c43ed2d959000c
mistvle <mistle.roblox@gmail.com>
2026-06-08 23:23:39 -0700

    A

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
   async execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
 
-    client.user.setActivity("over txrp3", {
+    client.user.setActivity("🌿 Powering Compact Customs", {
       type: 3 // WATCHING
     });
   }
