80b0361cce7c434e09d84fa1b8877ef4b8a9ae71
mistvle <mistle.roblox@gmail.com>
2026-06-08 23:14:01 -0700

    A

---
+++ commands/slash/tax.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder} = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("tax")
+    .setDescription("Calculate how much to charge based off Roblox tax.")
+    .addIntegerOption(option => option
+        .setName("amount")
+        .setDescription("Input the amount to tax.")
+        .setRequired(true)
+
+    ),
+
+    async execute (interaction) {
+        const amount = interaction.options.getInteger("amount");
+        const newAmount = Math.round(amount * 1.3);
+        await interaction.reply({
+  "flags": 32768,
+  "components": [
+    {
+      "type": 17,
+      "components": [
+        {
+          "type": 10,
+          "content": `To receive **$R$${amount}**, you must charge *R$${newAmount}**.`
+        }
+      ]
+    }
+  ]
+})
+    }
+}
